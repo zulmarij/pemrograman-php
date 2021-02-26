9f6fc9e5e140b4a5f5580eb0304f36c401d81912
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-02-27 06:48:51 +0700

    pemrograman php

---
+++ 5.Integer-Float.php
@@ -0,0 +1,28 @@
+<?php
+$a = 10;
+$b = 11;
+$hasil = $a * $b; //contoh perkalian
+echo $hasil;
+
+echo "<br>";
+
+$c = 1.5;
+$d = 2.5;
+$hasil = $c + $d; //contoh penjumlahan
+echo $hasil;
+
+echo "<br>";
+
+$e=10;
+$e++;
+echo $e; // menghasilkan nilai 11
+echo "<br>";
+$f = 10;
+$f--;
+echo $f; //menghasilkan nilai 9
+echo "<br>";
+$g = 10;
+$g++;
+$g++;
+echo $g;//akan menghasilkan nilai 12 karena menggunakan 2x operator increment
+echo "<br>";
