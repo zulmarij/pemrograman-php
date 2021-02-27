b0f848122ea36412be5775b6891c81db98b5c426
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-02-27 12:30:39 +0700

    pemrograman php

---
+++ 7.Boolean.php
@@ -0,0 +1,12 @@
+<?php
+$nilai1 = 10;
+$nilai2 = 5;
+$kondisi = $nilai1>$nilai2; //cek kondisi apakah bernilai true(benar) atau false(salah)
+var_dump($kondisi);
+
+echo "<br>";
+$a  = TRUE;
+$b  = "TRUE";
+var_dump($a);
+echo "<br>";
+var_dump($b);
